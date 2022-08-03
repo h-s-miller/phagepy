0a273bf37471865e528ab4d0f1e21085533b3693
H Miller <haleighmiller366@gmail.com>
2022-08-02 17:43:00 -0700

    unit tests updated, automated pytest on push

--- test/.ipynb_checkpoints/test_pp-checkpoint.py
+++ test/.ipynb_checkpoints/test_pp-checkpoint.py
@@ -18,30 +18,36 @@ def test_read_data():
     adata=pp.create_anndata(counts, meta)
 
     ## check if shape is correct ##
-    # since there are only 3/6 of the samples of the count matrix in the metadata
-    # the adata object should only have 3 observations
-    assert(adata.shape[0]==3 and adata.shape[1]==50), "Adata not read in correctly, shape is wrong"
+    assert(adata.shape[0]==8 and adata.shape[1]==6), "Adata not read in correctly, shape is wrong"
 
     ## check if the metadata read in correctly ##
-    assert(len(adata.obs.columns)==11), "Metadata not added to adata object"
+    assert(len(adata.obs.columns)==3), "Metadata not added to adata object"
     
     ## check if the transpose argument works correctly ##
     counts='./test/test_counts_transpose.csv'
     adata=pp.create_anndata(counts,meta, transpose=False)
-    assert(adata.shape[0]==3 and adata.shape[1]==50), "Adata not read in correctly when transpose=False, shape is wrong"
+    assert(adata.shape[0]==8 and adata.shape[1]==6), "Adata not read in correctly when transpose=False, shape is wrong"
 
-    
-def test_compute_expected_rpk():
+
+def test_filter_out_ctrl():
     ad=data_for_testing()
-    ctrl_ids=['AG9_R3_Demux2_S248_R1_001','B6_10C_R3_Demux2_S282_R1_001']
+    ad=pp.define_ctrl_set(ad, obs_key='group', obs_value='MockIP')
+    ad=pp.filter_out_ctrl_set(ad)
+    assert(ad.shape[0]==4 and ad.shape[1]==6, "Not filtering out controls properly, shape is wrong")
 
-    # pseudocount
-    ad.X=ad.X+1
-    # need to define control first
-    ad.uns['control_ids']=ctrl_ids # normally define_ctrl_set() fxn would do this, but i picked arbitrary obs to be ctrl for test
+
+def test_fold_change():
+    ad=data_for_testing()
+    ad=pp.define_ctrl_set(ad, obs_key='group', obs_value='MockIP')
     ad=pp.filter_out_ctrl_set(ad)
 
-    print(ad)
-    exp_rpk=pp.compute_expected_rpk(ad)
+    fc=pp.peptide_fold_change(ad)
+    assert(np.array_equal(fc.sum(axis=0),[2., 2., 8., 2., 8., 1.]))
 
-    print(exp_rpk.shape)
+def test_scaling_factor():
+    ad=data_for_testing()
+    ad=pp.define_ctrl_set(ad, obs_key='group', obs_value='MockIP')
+    ad=pp.filter_out_ctrl_set(ad)
+    
+    scaling=pp.compute_scaling_factor(ad, n=2)
+    assert(np.array_equal(scaling,[3., 3., 3., 3.]))
