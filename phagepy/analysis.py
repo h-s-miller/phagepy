663f0def5688a484602ed149c76172014eaf9639
haleigh miller <haleigh@strix.ucsf.edu>
2023-01-09 22:04:42 +0000

    nvm

--- phagepy/analysis.py
+++ phagepy/analysis.py
@@ -2,6 +2,34 @@ import pandas as pd
 import numpy as np
 import random
 
+AA2NA = {
+"A": list("GCT,GCC,GCA,GCG".split(",")),
+"R": list("CGT,CGC,CGA,CGG,AGA,AGG".split(",")),
+"N": list("AAT,AAC".split(",")),
+"D": list("GAT,GAC".split(",")),
+"C": list("TGT,TGC".split(",")),
+"Q": list("CAA,CAG".split(",")),
+"E": list("GAA,GAG".split(",")),
+"G": list("GGT,GGC,GGA,GGG".split(",")),
+"H": list("CAT,CAC".split(",")),
+"I": list("ATT,ATC,ATA".split(",")),
+"L": list("TTA,TTG,CTT,CTC,CTA,CTG".split(",")),
+"K": list("AAA,AAG".split(",")),
+"M": list("ATG".split(",")),
+"F": list("TTT,TTC".split(",")),
+"P": list("CCT,CCC,CCA,CCG".split(",")),
+"S": list("TCT,TCC,TCA,TCG,AGT,AGC".split(",")),
+"T": list("ACT,ACC,ACA,ACG".split(",")),
+"W": list("TGG".split(",")),
+"Y": list("TAT,TAC".split(",")),
+"V": list("GTT,GTC,GTA,GTG".split(",")),
+"*": list("TAA,TGA,TAG".split(","))}
+
+def aa2na(seq):
+    na_seq = [random.choice(AA2NA.get(c, ["---"])) for c in seq]
+    return "".join(na_seq)
+    hibit_seqs={} # make a dictionary-- fasta header:fasta seq
+
 def generate_SS_hits(ad, layer, ctrl_key, ctrl_value, z_cutoff=2):
     # take mean and standard deviation of the control group
     mu=np.mean(ad[ad.obs[ctrl_key]==ctrl_value].layers[layer],axis=0)
@@ -32,37 +60,10 @@ def generate_peptide_table(adata, map_file):
 
 
 def generate_alanine_lib_fastq(pep_table, out_file):
-    AA2NA = {
-    "A": list("GCT,GCC,GCA,GCG".split(",")),
-    "R": list("CGT,CGC,CGA,CGG,AGA,AGG".split(",")),
-    "N": list("AAT,AAC".split(",")),
-    "D": list("GAT,GAC".split(",")),
-    "C": list("TGT,TGC".split(",")),
-    "Q": list("CAA,CAG".split(",")),
-    "E": list("GAA,GAG".split(",")),
-    "G": list("GGT,GGC,GGA,GGG".split(",")),
-    "H": list("CAT,CAC".split(",")),
-    "I": list("ATT,ATC,ATA".split(",")),
-    "L": list("TTA,TTG,CTT,CTC,CTA,CTG".split(",")),
-    "K": list("AAA,AAG".split(",")),
-    "M": list("ATG".split(",")),
-    "F": list("TTT,TTC".split(",")),
-    "P": list("CCT,CCC,CCA,CCG".split(",")),
-    "S": list("TCT,TCC,TCA,TCG,AGT,AGC".split(",")),
-    "T": list("ACT,ACC,ACA,ACG".split(",")),
-    "W": list("TGG".split(",")),
-    "Y": list("TAT,TAC".split(",")),
-    "V": list("GTT,GTC,GTA,GTG".split(",")),
-    "*": list("TAA,TGA,TAG".split(","))}
-
-    def aa2na(seq):
-        na_seq = [random.choice(AA2NA.get(c, ["---"])) for c in seq]
-        return "".join(na_seq)
-        hibit_seqs={} # make a dictionary-- fasta header:fasta seq
     
     for p in pep_table.index:
         #add original seq to the dictionary 
-        hibit_seqs['{}_frag{}_ALAscanNULL'.format(pep_table.loc[p,'gene'],pep_table.loc[p,'fragment'])]=a2na(pep_table.loc[p,'seq'])
+        hibit_seqs['{}_frag{}_ALAscanNULL'.format(pep_table.loc[p,'gene'],pep_table.loc[p,'fragment'])]=aa2na(pep_table.loc[p,'seq'])
         
         #do alanine scan of the peptide
         for i in range(49): 
